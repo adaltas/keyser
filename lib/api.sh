546b6ff42257dfb11fc27ee402ae17731b38c1a0
David Worms <david@adaltas.com>
2023-12-01 09:26:26 +0100

    feat(certs): keys in hostname folder

--- lib/api.sh
+++ lib/api.sh
@@ -14,21 +14,21 @@ Usage:
   keyser <command>
 
 Available Commands:
-  ./keyser cacert <fqdn>
-  ./keyser cacert_view <fqdn>
-  ./keyser cert <fqdn>
-  ./keyser cert_check <fqdn> [<ca_file>]
-  ./keyser cert_check_from_file <fqdn_file> [<ca_file>]
-  ./keyser cert_view <fqdn>
-  ./keyser csr_create <fqdn>
-  ./keyser csr_sign <fqdn>
-  ./keyser csr_sign_from_file <fqdn>
-  ./keyser csr_view <fqdn>
+  keyser cacert <fqdn>
+  keyser cacert_view <fqdn>
+  keyser cert <fqdn>
+  keyser cert_check <fqdn> [<ca_file>]
+  keyser cert_check_from_file <cert_file> [<ca_file>]
+  keyser cert_view <fqdn>
+  keyser csr_create <fqdn>
+  keyser csr_sign <fqdn> [<ca_fqdn>]
+  keyser csr_sign_from_file <csr_file> [<ca_fqdn>]
+  keyser csr_view <fqdn>
 
 Example
-  ./keyser cacert domain.com
-  ./keyser cert test.domain.com
-  ./keyser cert_view test.domain.com
+  keyser cacert domain.com
+  keyser cert test.domain.com
+  keyser cert_view test.domain.com
 
 """
 }
@@ -43,26 +43,27 @@ Usage
 """
 }
 cacert(){
-  tld=$1
-  if [ -e $1 ]; then help_cacert; exit; fi
-  tld_dir=$VAULT_DIR/$(utils_reverse $tld)
+  fqdn=$1
+  if [ ! -n "$fqdn" ]; then echo 'FQDN is missing from arguments.'; help_cacert; exit 1; fi
+  fqdn_dir=$VAULT_DIR/$(utils_reverse $fqdn)
   # Validation
-  if [ -f "$tld_dir/ca.key.pem" ]; then  echo ''; echo '!!! cacert files already exists !!!'; echo ''; help; exit 1; fi
+  if [ -f "$fqdn_dir/ca.key.pem" ]; then  echo ''; echo '!!! cacert files already exists !!!'; echo ''; help; exit 1; fi
   # Prepare configuration template
-  mkdir -p $tld_dir
-  cp -rp $pwd/ca.cnf $tld_dir/ca.cnf
-  sed -i "s|<commonName>|$tld|" $tld_dir/ca.cnf
+  mkdir -m 700 -p $fqdn_dir
+  cp -rp $pwd/ca.cnf $fqdn_dir/ca.cnf
+  sed -i "s|<commonName>|$fqdn|" $fqdn_dir/ca.cnf
   # RSA Private key (create "ca.key.pem")
-  openssl genrsa -out $tld_dir/ca.key.pem 2048
+  openssl genrsa -out $fqdn_dir/key.pem 2048
   # Self-signed (with the key previously generated) root CA certificate (create "ca.cert.pem")
+  # man: The req command primarily creates and processes certificate requests in PKCS#10 format.
   # "/C=FR/ST=IDF/L=Paris/O=Adaltas/CN=adaltas.com/emailAddress=david@adaltas.com"
   openssl req -x509 -new -sha256 \
-    -config $tld_dir/ca.cnf \
-    -subj "/C=FR/O=ADALTAS/L=Paris/CN=$tld" \
-    -key $tld_dir/ca.key.pem -days 7300 \
-    -out $tld_dir/ca.cert.pem
-  echo 'Certificate key created:' $tld_dir/ca.key.pem
-  echo 'Certificate authority created:' $tld_dir/ca.cert.pem
+    -config $fqdn_dir/ca.cnf \
+    -subj "/C=FR/O=ADALTAS/L=Paris/CN=$fqdn" \
+    -key $fqdn_dir/key.pem -days 7300 \
+    -out $fqdn_dir/cert.pem
+  echo 'Certificate key created:' $fqdn_dir/key.pem
+  echo 'Certificate authority created:' $fqdn_dir/cert.pem
 }
 
 help_cacert_view(){
@@ -76,10 +77,10 @@ Usage
 }
 cacert_view(){
   fqdn=$1
-  if [ -e $fqdn ]; then help_cacert_view; exit 1; fi
-  tld_dir=$VAULT_DIR/$(utils_reverse $fqdn)
+  if [ ! -n "$fqdn" ]; then echo 'FQDN is missing from arguments.'; help_cacert_view; exit 1; fi
+  fqdn_dir=$VAULT_DIR/$(utils_reverse $fqdn)
   openssl x509 -noout -text -fingerprint \
-    -in $tld_dir/ca.cert.pem
+    -in $fqdn_dir/cert.pem
 }
 
 help_cert(){
@@ -90,13 +91,12 @@ Description
 Usage
   keyser cert <fqdn>
 
-Generate the certificate and private key for a give hostname. The command
-combine the csr_create and csr_sign commands for conveniency.
+Generate the certificate and private key for a give hostname. The command combine the csr_create and csr_sign commands for conveniency.
 """
 }
 cert(){
   fqdn=$1
-  if [ -e $fqdn ]; then help_cert; exit 1; fi
+  if [ ! -n "$fqdn" ]; then echo 'FQDN is missing from arguments.'; help_cert; exit 1; fi
   csr_create $fqdn
   csr_sign $fqdn
 }
@@ -107,29 +107,26 @@ Description
   Check a hostname certificate against the certificate authority.
 
 Usage
-  keyser cert_check <fqdn>
+  keyser cert_check <fqdn> [<ca_file>]
 
-Internally, the command localize the certificate inside its store. Then, it call
-the cert_check_from_file command.
+Internally, the command localize the certificate inside its store. Then, it call the cert_check_from_file command.
 """
 }
 cert_check(){
-  if [ -e $1 ]; then help_cert_check; exit 1; fi
   fqdn=$1
-  tld=${fqdn#*.}
-  hostname=${fqdn%%.*}
-  tld_dir=$VAULT_DIR/$(utils_reverse $tld)
-  cert_file=$tld_dir/$hostname.cert.pem
-  cert_check_from_file $cert_file
+  if [ ! -n "$fqdn" ]; then echo 'FQDN is missing from arguments.'; help_cert_check; exit 1; fi
+  fqdn_dir=$VAULT_DIR/$(utils_reverse $fqdn)
+  cert_file=$fqdn_dir/cert.pem
+  cert_check_from_file $cert_file $2
 }
 
-help_cert_check(){
+help_cert_check_from_file(){
   echo """
 Description
   Check a hostname certificate against the certificate authority.
 
 Usage
-  keyser cert_check_from_file <fqdn_file> [<ca_file>]
+  keyser cert_check_from_file <cert_file> [<ca_file>]
 
 Internally, the command localize the certificate inside its store. Then, it call
 the cert_check_from_file command.
@@ -137,17 +134,22 @@ the cert_check_from_file command.
 }
 cert_check_from_file(){
   cert_file=$1
-  if [ ! -f $cert_file ]; then echo 'Certificate file does not exist'; exit 1; fi
-  fqdn=`openssl x509 -noout -subject -in $cert_file | sed -n '/^subject/s/^.*CN\s*=\s*//p'`
-  tld=${fqdn#*.}
-  hostname=${fqdn%%.*}
-  tld_dir=$VAULT_DIR/$(utils_reverse $tld)
-  ca_file=${2:-"$tld_dir/ca.cert.pem"}
-  echo $ca_file
-  echo $cert_file
+  if [ ! -n "$cert_file" ]; then echo 'Certificate file path is missing from arguments.'; help_cert_check_from_file; exit 1; fi
+  if [ ! -f $cert_file ]; then echo "Certificate file does not exist: \"$cert_file\"."; exit 1; fi
+  ca_file=$2
+  echo ca_file_before: $ca_file
+  if [ ! -n "$ca_file" ]; then 
+    fqdn=`openssl req -noout -subject -in  $csr_file | sed -n '/^subject/s/^.*CN\s*=\s*//p'`;
+    ca_fqdn=${fqdn#*.}
+    ca_fqdn_dir=$VAULT_DIR/$(utils_reverse $ca_fqdn)
+    ca_file=$ca_fqdn_dir/cert.pem
+  fi
+  echo ca_file_after: $ca_file
+  echo cert_file: $cert_file
   openssl verify \
     -CAfile $ca_file \
     $cert_file \
+    >/dev/null \
     2>/dev/null
   if [ $? == 0 ]; then
     echo 'Certificate is valid.'
@@ -165,103 +167,141 @@ Description
 Usage
   keyser cert_view <fqdn>
 
-Generate the certificate and private key for a give hostname. The command
-combine the csr_create and csr_sign commands for conveniency.
+Print the detailed information of a Certificate Signing Request (CSR) file.
 """
 }
 cert_view(){
-  if [ -z "$1" ]; then help_cert_view; exit 1; fi
   fqdn=$1
-  tld=${fqdn#*.}
-  hostname=${fqdn%%.*}
-  tld_dir=$VAULT_DIR/$(utils_reverse $tld)
+  if [ ! -n "$fqdn" ]; then echo 'FQDN is missing from arguments.'; help_cert_view; exit 1; fi
+  # tld=${fqdn#*.}
+  # hostname=${fqdn%%.*}
+  fqdn_dir=$VAULT_DIR/$(utils_reverse $fqdn)
   #domain=`openssl x509 -noout -subject -in ca.cert.pem | sed -n '/^subject/s/^.*CN=//p'`
   #shortname=${fqdn%".$domain"}
   # shortname=`echo $1 | sed 's/\([[:alnum:]]\)\..*/\1/'`
   openssl x509 -text -fingerprint \
-    -in $tld_dir/${hostname}.cert.pem
+    -in $fqdn_dir/cert.pem
 }
 
 help_csr_create(){
   echo """
 Description
-  Create a certificate signing request
+  Create a certificate signing request.
 
 Usage
-  keyser cert <fqdn>
+  keyser csr_create <fqdn>
 
-Generate a the key and the certificate signing request.
+Generate a key and its certificate signing request.
 """
 }
 csr_create(){
   fqdn=$1
-  if [ -e $fqdn ]; then help_csr_create; exit 1; fi
+  if [ ! -n "$fqdn" ]; then echo 'FQDN is missing from arguments.'; help_csr_create; exit 1; fi
   # domain=`openssl x509 -noout -subject -in $VAULT_DIR/ca.cert.pem | sed -n '/^subject/s/^.*CN=//p'`
-  # hostname=${fqdn%".$domain"}
-  tld=${fqdn#*.}
-  hostname=${fqdn%%.*}
-  tld_dir=$VAULT_DIR/$(utils_reverse $tld)
-  if [ ! -f $tld_dir/ca.key.pem ]; then echo 'Run `./keyser cacert` first.'; exit 1; fi
+  fqdn_dir=$VAULT_DIR/$(utils_reverse $fqdn)
   # to view the CSR: `openssl req -in toto.cert.csr -noout -text`
   # Sign the CSR (create "hadoop.cert.pem")
+  mkdir -m 700 -p $fqdn_dir
   openssl req -newkey rsa:2048 -sha256 -nodes \
-    -out $tld_dir/${hostname}.cert.csr \
-    -keyout $tld_dir/${hostname}.key.pem \
+    -out $fqdn_dir/cert.csr \
+    -keyout $fqdn_dir/key.pem \
     -subj "/C=FR/O=ADALTAS.CLOUD/L=Paris/CN=${fqdn}" \
     2>/dev/null
-  echo 'Key created in:' $tld_dir/${hostname}.key.csr
-  echo 'CSR created in:' $tld_dir/${hostname}.cert.csr
+  [ $? != 0 ] && exit 1
+  echo 'Key created in:' $fqdn_dir/key.pem
+  echo 'CSR created in:' $fqdn_dir/cert.csr
 }
 
+help_csr_sign(){
+  echo """
+Description
+  Sign a CSR givent its path.
+
+Usage
+  keyser csr_sign <fqdn> [<ca_fqdn>]
+
+Generate a certificate signing request (CSR) for a managed FQDN. Internally, it calls the "csr_sign_from_file" command.
+"""
+}
 csr_sign(){
-  if [ -z "$1" ]; then help; exit 1; fi
   fqdn=$1
-  tld=${fqdn#*.}
-  hostname=${fqdn%%.*}
-  csr_file=$VAULT_DIR/$tld/$hostname.cert.csr
-  csr_sign_from_file $csr_file
+  if [ ! -n "$fqdn" ]; then echo 'FQDN is missing from arguments.'; help_csr_sign; exit 1; fi
+  fqdn_dir=$VAULT_DIR/$(utils_reverse $fqdn)
+  csr_sign_from_file $fqdn_dir/cert.csr $2
 }
 
+help_csr_sign_from_file(){
+  echo """
+Description
+  Sign a CSR givent its path.
+
+Usage
+  keyser csr_sign_from_file <csr_file> [<ca_fqdn>]
+
+Generate a certificate signing request (CSR) for a given file. The Certificate Authority FQDN is optionnal. When not provided, it is extracted from the CSR subject.
+"""
+}
 csr_sign_from_file(){
   csr_file=$1
-  if [ ! -f $csr_file ]; then echo 'CSR file does not exist'; exit 1; fi
-  fqdn=`openssl req -noout -subject -in  $csr_file | sed -n '/^subject/s/^.*CN\s*=\s*//p'`
-  tld=${fqdn#*.}
-  hostname=${fqdn%%.*}
-  tld_dir=$VAULT_DIR/$(utils_reverse $tld)
-  # Copy the csr file into the vault directory unless already there
-  if [ $csr_file != $tld_dir/${csr_file##*/} ]; then
-    cp -rp $csr_file $tld_dir/${csr_file##*/}
+  if [ ! -f $csr_file ]; then echo "CSR file does not exist: \"$csr_file\"."; help_csr_sign_from_file; exit 1; fi
+  fqdn=`openssl req -noout -subject -in  $csr_file | sed -n '/^subject/s/^.*CN\s*=\s*//p'`;
+  ca_fqdn=$2
+  if [ -e $ca_fqdn ]; then 
+    ca_fqdn=${fqdn#*.}
+  fi
+  fqdn_dir=$VAULT_DIR/$(utils_reverse $fqdn)
+  ca_fqdn_dir=$VAULT_DIR/$(utils_reverse $ca_fqdn)
+  if [ $csr_file != "$fqdn_dir/${csr_file##*/}" ]; then
+    cp -rp $csr_file $fqdn_dir/${csr_file##*/}
   fi
   # Sign the CSR
   openssl x509 -req -sha256 -days 7300 \
-    -CA $tld_dir/ca.cert.pem -CAkey $tld_dir/ca.key.pem \
-    -CAcreateserial -CAserial $tld_dir/ca.seq \
+    -CA $ca_fqdn_dir/cert.pem -CAkey $ca_fqdn_dir/key.pem \
+    -CAcreateserial -CAserial $ca_fqdn_dir/ca.seq \
     -extfile $pwd/sign.cnf \
     -in $csr_file \
-    -out $tld_dir/${hostname}.cert.pem \
+    -out $fqdn_dir/cert.pem \
     2>/dev/null
-  echo 'Certificate created in:' $tld_dir/${hostname}.cert.pem
+  echo 'Certificate created in:' $fqdn_dir/cert.pem
 }
 
+help_csr_view(){
+  echo """
+Description
+  Print a CSR.
+
+Usage
+  keyser csr_view <fqdn>
+
+Print the detailed information of a Certificate Signing Request (CSR) stored inside the vault.
+"""
+}
 csr_view(){
   fqdn=$1
-  if [ -z "$1" ]; then help; exit 1; fi
-  tld=${fqdn#*.}
-  hostname=${fqdn%%.*}
-  tld_dir=$VAULT_DIR/$(utils_reverse $tld)
-  # domain=`openssl x509 -noout -subject -in $VAULT_DIR/ca.cert.pem | sed -n '/^subject/s/^.*CN=//p'`
-  # shortname=${fqdn%".$domain"}
+  if [ ! -n "$fqdn" ]; then echo 'FQDN is missing from arguments.'; help_csr_view; exit 1; fi
+  fqdn_dir=$VAULT_DIR/$(utils_reverse $fqdn)
+  if [ ! -f "$fqdn_dir/cert.csr" ]; then echo "CSR file for domain \"$fqdn\" does not exist."; help_csr_view; exit 1; fi
   openssl req -noout -text \
-    -in $tld_dir/${hostname}.cert.csr
+    -in $fqdn_dir/cert.csr
 }
 
 utils_reverse(){
   IFS=. read -ra line <<< "$1"
-  let x=${#line[@]}-1; 
-  while [ "$x" -ge 0 ]; do 
-        echo -n "${line[$x]}"; 
-        [ $x != 0 ] && echo -n '.';
-        let x--; 
+  let x=${#line[@]}-1;
+  while [ "$x" -ge 0 ]; do
+    echo -n "${line[$x]}";
+    [ $x != 0 ] && echo -n '.';
+    let x--;
+    echo -n ''
   done
+}
+
+utils_tld(){
+  fqdn=$1
+  echo -n ${fqdn#*.}
+}
+
+utils_domain(){
+  fqdn=$1
+  echo -n ${fqdn%%.*}
 }
--- lib/test.sh
+++ lib/test.sh
@@ -9,128 +9,182 @@ function test_cacert {
   VAULT_DIR='../tmp/test_cacert'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
-  [ -f "$VAULT_DIR/com.domain/ca.cert.pem" ] || false
-  [ -f "$VAULT_DIR/com.domain/ca.key.pem" ] || false
+  res=`cacert domain.com`
+  [ -f "$VAULT_DIR/com.domain/cert.pem" ] || exit 1
+  [ -f "$VAULT_DIR/com.domain/key.pem" ] || exit 1
+  [ $? != 0 ] && exit 1
+  echo "$res" | grep 'Certificate key created:' > /dev/null 
+  echo "$res" | grep 'Certificate authority created:' > /dev/null 
 }
 
 function test_cacert_view {
   VAULT_DIR='../tmp/test_cacert_view'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com > /dev/null
   # Validate certificate
   res=`cacert_view domain.com`
-  echo $res | grep 'Certificate:' > /dev/null 
+  [ $? != 0 ] && exit 1
+  echo "$res" | grep 'Certificate:' > /dev/null
+  echo "$res" | grep 'Subject: C = FR, O = ADALTAS, L = Paris, CN = domain.com' > /dev/null
 }
 
 function test_cert {
   VAULT_DIR='../tmp/test_cert'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com > /dev/null
   # Create a certificate
-  cert test.domain.com
-  [ -f "$VAULT_DIR/com.domain/test.cert.pem" ] || false
-  [ -f "$VAULT_DIR/com.domain/test.key.pem" ] || false
+  res=`cert test.domain.com`
+  [ $? != 0 ] && exit 1
+  [ -f "$VAULT_DIR/com.domain.test/cert.pem" ] || exit 1
+  [ -f "$VAULT_DIR/com.domain.test/key.pem" ] || exit 1
+  echo "$res" | grep 'Key created in:' > /dev/null
+  echo "$res" | grep 'CSR created in:' > /dev/null
+  echo "$res" | grep 'Certificate created in:' > /dev/null
 }
 
 function test_cert_check {
   VAULT_DIR='../tmp/test_cert'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com > /dev/null
   # Create a certificate
-  cert test.domain.com
+  cert test.domain.com > /dev/null
   # Validate certificate
   res=`cert_check test.domain.com`
-  echo $res | grep 'Certificate is valid.' > /dev/null 
+  [ $? != 0 ] && exit 1
+  echo "$res" | grep 'Certificate is valid.' > /dev/null 
 }
 
-function test_cert_check_from_file {
-  VAULT_DIR='../tmp/test_cert_check_from_file'
+function test_cert_check_from_file_no_ca_file {
+  VAULT_DIR='../tmp/test_cert_check_from_file_no_ca_file'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com > /dev/null
   # Create a certificate
-  cert test.domain.com
+  cert test.domain.com > /dev/null
   # Validate certificate
-  res=`cert_check_from_file "$VAULT_DIR/com.domain/test.cert.pem"`
-  echo $res | grep 'Certificate is valid.' > /dev/null 
+  res=`cert_check_from_file $VAULT_DIR/com.domain.test/cert.pem`
+  [ $? != 0 ] && exit 1
+  echo "$res" | grep 'Certificate is valid.' > /dev/null 
+}
+
+function test_cert_check_from_file_with_ca_file {
+  VAULT_DIR='../tmp/test_cert_check_from_file_with_ca_file'
+  rm -rf $VAULT_DIR
+  # Generate a certificate authority
+  cacert domain.com > /dev/null
+  # Create a certificate
+  cert test.domain.com > /dev/null
+  # Move parent certificate
+  mv $VAULT_DIR/com.domain/cert.pem $VAULT_DIR/parent.cert.pem
+  # Validate certificate
+  res=`cert_check_from_file $VAULT_DIR/com.domain.test/cert.pem $VAULT_DIR/parent.cert.pem`
+  [ $? != 0 ] && exit 1
+  echo "$res" | grep 'Certificate is valid.' > /dev/null 
 }
 
 function test_cert_check_from_file_invalid {
   VAULT_DIR='../tmp/test_cert_check_from_file_invalid'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com > /dev/null
   # Create a certificate
-  cert test.domain.com
+  cert test.domain.com > /dev/null
   # create an invalid certificate
-  cacert invalid.com
-  cp -rp "$VAULT_DIR/invalid.com/ca.cert.pem" "$VAULT_DIR/com.domain/ca.cert.pem"
+  cacert invalid.com > /dev/null
+  cp -rp "$VAULT_DIR/com.invalid/cert.pem" "$VAULT_DIR/com.domain/cert.pem"
   # Validate certificate
-  res=`cert_check_from_file "$VAULT_DIR/com.domain/test.cert.pem"`
-  echo $res | grep 'Certificate is not valid.' > /dev/null 
+  res=`cert_check_from_file "$VAULT_DIR/com.domain.test/cert.pem"`
+  [ $? != 1 ] && exit 1
+  echo "$res" | grep 'Certificate is not valid.' > /dev/null 
 }
 
 function test_cert_view {
   VAULT_DIR='../tmp/test_cert_view'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com > /dev/null
   # Create a certificate
-  cert test.domain.com
+  cert test.domain.com > /dev/null
+  # View a certificate
   res=`cert_view test.domain.com`
-  echo $res | grep 'Certificate:' > /dev/null 
+  [ $? != 0 ] && exit 1
+  echo "$res" | grep 'Certificate:' > /dev/null 
 }
 
-function test_csr_create {
+function test_csr_create { # _discover_domain
   VAULT_DIR='../tmp/test_csr_create'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com >/dev/null
   # Create a certificate
-  csr_create test.domain.com
-  [ -f "$VAULT_DIR/com.domain/test.cert.csr" ] || false
+  res=`csr_create test.domain.com`
+  [ $? != 0 ] && (echo $res && exit 1)
+  [ -f "$VAULT_DIR/com.domain.test/key.pem" ] || exit 1
+  [ -f "$VAULT_DIR/com.domain.test/cert.csr" ] || exit 1
+  echo "$res" | grep 'Key created in:' > /dev/null 
+  echo "$res" | grep 'CSR created in:' > /dev/null 
 }
 
-function test_csr_sign {
+function test_csr_sign { # _discover_domain
   VAULT_DIR='../tmp/test_csr_sign'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com >/dev/null
   # Create a certificate
-  csr_create test.domain.com
-  csr_sign test.domain.com
-  [ -f "$VAULT_DIR/com.domain/test.cert.pem" ] || false
+  csr_create test.domain.com >/dev/null
+  # Sign the certificate
+  res=`csr_sign test.domain.com`
+  [ $? != 0 ] && exit 1
+  [ -f "$VAULT_DIR/com.domain.test/cert.pem" ] || exit 1
+  echo "$res" | grep 'Certificate created in:' > /dev/null
 }
 
 function test_csr_sign_from_file {
   VAULT_DIR='../tmp/test_csr_sign_from_file'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com >/dev/null
   # Create a certificate
-  csr_create test.domain.com
-  csr_sign_from_file "$VAULT_DIR/com.domain/test.cert.csr"
-  [ -f "$VAULT_DIR/com.domain/test.cert.pem" ] || false
+  csr_create test.domain.com >/dev/null
+  # Sign the certificate
+  res=`csr_sign_from_file "$VAULT_DIR/com.domain.test/cert.csr"`
+  [ $? != 0 ] && exit 1
+  [ -f "$VAULT_DIR/com.domain.test/cert.pem" ] || exit 1
+  echo "$res" | grep 'Certificate created in:' > /dev/null
 }
 
 function test_csr_view {
   VAULT_DIR='../tmp/test_csr_view'
   rm -rf $VAULT_DIR
   # Generate a certificate authority
-  cacert domain.com
+  cacert domain.com >/dev/null
   # Create a certificate
-  csr_create test.domain.com
+  csr_create test.domain.com >/dev/null
+  # View the certificate
   res=`csr_view test.domain.com`
-  echo $res | grep 'Certificate Request:' > /dev/null 
+  [ $? != 0 ] && exit 1
+  echo "$res" | grep 'Certificate Request:' > /dev/null 
 }
 
 function test_utils_reverse {
   res=`utils_reverse test.domain.com`
-  [ $res == 'com.domain.test' ] || false
+  [ $? != 0 ] && exit 1
+  [ $res == 'com.domain.test' ] || exit 1
+}
+
+function test_utils_tld {
+  res=`utils_tld test.domain.com`
+  [ $? != 0 ] && exit 1
+  [ $res == 'domain.com' ] || exit 1
+}
+
+function test_utils_domain {
+  res=`utils_domain test.domain.com`
+  [ $? != 0 ] && exit 1
+  [ $res == 'test' ] || exit 1
 }
 
 tests="""
@@ -138,7 +192,8 @@ test_cacert
 test_cacert_view
 test_cert
 test_cert_check
-test_cert_check_from_file
+test_cert_check_from_file_no_ca_file
+test_cert_check_from_file_with_ca_file
 test_cert_check_from_file_invalid
 test_cert_view
 test_csr_create
@@ -146,13 +201,15 @@ test_csr_sign
 test_csr_sign_from_file
 test_csr_view
 test_utils_reverse
+test_utils_tld
+test_utils_domain
 """
 
 for test in $tests; do
   if ! [[ $test = \#* ]]; then
     # echo ''; echo - $test \(skipped\)
-    echo ''; echo - $test
-    (! $test ) && echo 'KO' && exit 1
+    echo - $test
+    ( $test ) || (echo 'KO' && exit 1)
   fi
 done
 echo ''
