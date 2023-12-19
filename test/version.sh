a95ff95de6018d52a6d12e34b668dd0b20d3ea33
David Worms <david@adaltas.com>
2023-12-19 22:30:16 +0100

    feat: new version command

---
+++ test/version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# set -e
+cd `dirname "${BASH_SOURCE}"`
+. ../keyser
+
+function test_version {
+  # Generate a certificate authority
+  res=`version | grep -e '\d\d*\.\d\d*\.\d\d*'`
+  [ $? != 0 ] && exit 1
+  true
+}
+
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+  echo -n "$0: "
+  (test_version) && echo 'OK' || echo 'KO'
+fi
