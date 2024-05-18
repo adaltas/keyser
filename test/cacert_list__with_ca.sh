1ef967d5afa27c7b7cd4b2b5170f97e240500a62
David Worms <david@adaltas.com>
2024-05-18 16:51:14 +0200

    feat: new cacert list command

---
+++ test/cacert_list__with_ca.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# set -e
+cd `dirname "${BASH_SOURCE}"`
+. ../keyser
+
+function test_cacert_list__with_ca {
+  KEYSER_VAULT_DIR='../tmp/test_cacert_list__with_ca'
+  KEYSER_GPG_MODE=
+  KEYSER_GPG_PASSPHRASE=
+  rm -rf $KEYSER_VAULT_DIR
+  # Generate a certificate authority
+  cacert domain-1.com >/dev/null
+  # Create an intermediate certificates
+  cert domain-2.com domain-1.com >/dev/null
+  cert domain-3.com domain-2.com >/dev/null
+  # # Create a leaf certificate
+  cert domain-4.com domain-3.com >/dev/null
+  # List all certificates from the vault
+  res=`cacert_list`
+  [[ $? != 0 ]] && exit 1
+  echo "$res" | grep 'domain-1.com' > /dev/null
+}
+
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+  echo -n "$0: "
+  (test_cacert_list__with_ca) && echo 'OK' || echo 'KO'
+fi
