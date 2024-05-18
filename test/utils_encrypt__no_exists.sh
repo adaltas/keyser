0e3819e300f35cef4570afe191a923ccdc8d475c
David Worms <david@adaltas.com>
2024-05-18 18:24:56 +0200

    feat: encrypt source validation

---
+++ test/utils_encrypt__no_exists.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# set -e
+cd `dirname "${BASH_SOURCE}"`
+. ../keyser
+
+function test_utils_encrypt__no_exists {
+  KEYSER_VAULT_DIR='../tmp/test_utils_encrypt__no_exists'
+  KEYSER_GPG_MODE=symmetric
+  KEYSER_GPG_PASSPHRASE=secret
+  mkdir -p $KEYSER_VAULT_DIR
+  res=`utils_encrypt $KEYSER_VAULT_DIR/does_not_exists $KEYSER_VAULT_DIR/target`
+  [[ $? == 0 ]] && exit 1
+  echo "$res" | grep 'No such file to encrypt.' > /dev/null
+}
+
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+  echo -n " s$0: "
+  (test_utils_encrypt__no_exists) && echo 'OK' || echo 'KO'
+fi
