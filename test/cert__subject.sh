03ccac35c498fad8bf32a96ac0cf0853da62ba04
David Worms <david@adaltas.com>
2024-05-21 22:19:09 +0200

    test: fix naming

--- test/cert__subject.sh
+++ test/cert__subject.sh
@@ -4,8 +4,8 @@
 cd `dirname "${BASH_SOURCE}"`
 . ../keyser
 
-test_cert__san() {
-  KEYSER_VAULT_DIR='../tmp/test_cert__san'
+test_cert__subject() {
+  KEYSER_VAULT_DIR='../tmp/test_cert__subject'
   KEYSER_GPG_MODE=
   KEYSER_GPG_PASSPHRASE=
   rm -rf $KEYSER_VAULT_DIR
@@ -20,5 +20,5 @@ test_cert__san() {
 
 if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
   echo -n "$0: "
-  (test_cert__san) && echo 'OK' || echo 'KO'
+  (test_cert__subject) && echo 'OK' || echo 'KO'
 fi
