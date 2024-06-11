1748e1a3c55b2976a8ab9f67498aaf9048c9f7b5
David Worms <david@adaltas.com>
2024-06-11 02:35:57 +0200

    feat: cert_export create option

---
+++ test/cert_export__create.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd `dirname "${BASH_SOURCE}"`
+. ../keyser
+
+function test {
+  KEYSER_VAULT_DIR='../tmp/cert_view'
+  KEYSER_GPG_MODE=
+  KEYSER_GPG_PASSPHRASE=
+  rm -rf $KEYSER_VAULT_DIR
+  # Generate a certificate authority
+  cacert -c FR -e no-reply@domain -l P -o O domain.com > /dev/null
+  # Create a certificate
+  cert test.domain.com > /dev/null
+  # View a certificate
+  res=`cert_export -c test.domain.com $KEYSER_VAULT_DIR/some/target`
+  [[ $? != 0 ]] && exit 1
+  [[ -f $KEYSER_VAULT_DIR/some/target/com.domain.test.cert.pem ]] || exit 1
+  [[ -f $KEYSER_VAULT_DIR/some/target/com.domain.test.key.pem ]] || exit 1
+}
+
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+  echo -n "$0: "
+  (test) && echo 'OK' || echo 'KO'
+fi
