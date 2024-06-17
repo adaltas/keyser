99c79ffcb88e2c2419824a4d930d74fd766c3ed7
David Worms <david@adaltas.com>
2024-06-17 15:03:35 +0200

    fix: cert check with gpg

---
+++ test/cert_check__gpg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd `dirname "${BASH_SOURCE}"`
+. ../keyser
+
+function test {
+  KEYSER_VAULT_DIR='../tmp/cert'
+  KEYSER_GPG_PASSPHRASE=secret
+  rm -rf $KEYSER_VAULT_DIR
+  # Generate a certificate authority
+  cacert -c FR -e no-reply@domain -l P -o O domain.com > /dev/null
+  # Create a certificate
+  cert test.domain.com > /dev/null
+  # Validate certificate
+  res=`cert_check test.domain.com`
+  [[ $? != 0 ]] && exit 1
+  echo "$res" | grep 'Certificate is valid.' > /dev/null || exit 1
+  [[ -f $KEYSER_VAULT_DIR/com.domain.test/key.pem ]] && exit 1
+  true
+}
+
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+  echo -n "$0: "
+  (test) && echo 'OK' || echo 'KO'
+fi
