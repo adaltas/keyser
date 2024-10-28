ac6889df0858217c16a672ebadf7038d4116241b
Eliott Morcillo <eliott@adaltas.com>
2024-10-28 12:41:36 +0100

    feat: doc + test for wildcard support

---
+++ test/cert__wildcard.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd `dirname "${BASH_SOURCE}"`
+. ../keyser
+
+function test {
+  KEYSER_VAULT_DIR='../tmp/cert__wildcard'
+  KEYSER_GPG_PASSPHRASE=
+  rm -rf $KEYSER_VAULT_DIR
+  # Generate a certificate authority
+  cacert -c FR -e no-reply@domain -l P -o O domain.com > /dev/null
+  # Create a wildcard certificate
+  res=`cert "*.domain.com"`
+  [[ $? != 0 ]] && exit 1
+  [[ -f "$KEYSER_VAULT_DIR/com.domain.*/ca.crt" ]] || exit 1
+  [[ -f "$KEYSER_VAULT_DIR/com.domain.*/cert.pem" ]] || exit 1
+  [[ -f "$KEYSER_VAULT_DIR/com.domain.*/key.pem" ]] || exit 1
+  echo "$res" | grep 'Key created in:' > /dev/null || exit 1
+  echo "$res" | grep 'CSR created in:' > /dev/null || exit 1
+  echo "$res" | grep 'Certificate created in:' > /dev/null || exit 1
+}
+
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+  echo -n "$0: "
+  (test) && echo 'OK' || echo 'KO'
+fi
