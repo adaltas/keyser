c8bb21c97240d9acbad596a74b591a17c7279230
Paul-Adrien Cordonnier <pa.cordonnier@gmail.com>
2025-11-13 17:39:51 +0100

    fix: field name when using multiples IP in SAN (#6)

--- test/csr_create__san.sh
+++ test/csr_create__san.sh
@@ -10,12 +10,12 @@ function test {
   # Generate a certificate authority
   cacert -c FR -e no-reply@domain -l P -o O domain.com >/dev/null
   # Create a certificate
-  csr_create -d domain.com,test.domain.com -a 127.0.0.1 -c FR -e no-reply@domain -l P -o O test.domain.com > /dev/null
+  csr_create -d domain.com,test.domain.com -a 127.0.0.1,10.10.10.1 -c FR -e no-reply@domain -l P -o O test.domain.com > /dev/null
   # SAN Validation
   res=$(openssl req -text -in $KEYSER_VAULT_DIR/com.domain.test/cert.csr)
   [[ $? != 0 ]] && (echo $res && return 1)
   echo "$res" | grep 'X509v3 Subject Alternative Name:' > /dev/null || return 1
-  echo "$res" | grep 'DNS:domain.com, DNS:test.domain.com, IP Address:127.0.0.1' > /dev/null || return 1
+  echo "$res" | grep 'DNS:domain.com, DNS:test.domain.com, IP Address:127.0.0.1, IP Address:10.10.10.1' > /dev/null || return 1
 }
 
 if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
